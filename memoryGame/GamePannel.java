8a4cc5e71ddc3f4582248c1f5d87d88d4ab62363
okatticus <apporva.hm@gmail.com>
2017-03-22 20:46:39 +0530

    Update GamePannel.java

--- memoryGame/GamePannel.java
+++ memoryGame/GamePannel.java
@@ -2,12 +2,8 @@ package memoryGame;
 
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -15,11 +11,11 @@ import javax.swing.border.LineBorder;
 
 /**
  *
- * @author srb
+ * @author apoorva
  */
 public class GamePannel extends javax.swing.JPanel {
 
-    int win=0,levelwin,level,maxlevel=2;
+    int win=0,levelwin=0,level=1,maxlevel=3,score=0;
     
     Border darkborder = new LineBorder(Color.DARK_GRAY, 1);
     Border lightborder = new LineBorder(Color.lightGray, 1);
@@ -30,16 +26,15 @@ public class GamePannel extends javax.swing.JPanel {
     CardLayout card=new CardLayout();//layout for controlPannel
   
     //game variables
-    int maxpics=8,a,b,c,d,e,i;
+    int maxpics=8,a,b,c,d,e,i,ans=0,pictime=800,lock=0;
+    Timer picTimer;
     
     public GamePannel() {
         initComponents();
         myinit();
     }//constructor
     
-    public void updateBox(){
-        
-    }
+   
     
     public final void myinit(){
         setSize(820, 620);//our standard game pannel size
@@ -51,97 +46,135 @@ public class GamePannel extends javax.swing.JPanel {
         controlPannel.setOpaque(false);//to make buttons transparent
         controlPannel.setFocusTraversalKeysEnabled(false);
         imageLabel.setBounds(0,0,588,288);
-        //imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/a.png")));
-        //controlPannel.add(imageLabel);
+        controlPannel.add(imageLabel);
         
         initBoard();
    
     }//my init
     
-    public boolean wincheck(){
+    public void gameFinish(){
         levelwin=1;
         if(levelwin==1){
             if(level<maxlevel){
                 JOptionPane.showMessageDialog(controlPannel,"Move to next level","Level Complete",JOptionPane.INFORMATION_MESSAGE);
-                    level=2;
-                    maxpics=12;
+                    score+=level*10;
+                    level++;
+                    maxpics+=2;
+                    pictime-=100;
                     initBoard();
             }
             else if(level==maxlevel){
                 win=1;
+                score+=level*10;
                 JOptionPane.showMessageDialog(controlPannel,"Yippee you won the game","Game Complete",JOptionPane.INFORMATION_MESSAGE);
             }   
         }
-        return true;
-    }// return true if level completes
+    }//called on finish of a level
     
     public void initBoard(){
-        ImageTry it=new ImageTry();
-        it.run();
+        ansTextField.setVisible(false);
+        submitButton.setVisible(false);
+        questionLabel.setVisible(false);
+        levelLabel.setText("Level : "+level+"/"+maxlevel);
+        questionLabel.setText("how many times this pic appeared :");
+        a=0;b=0;c=0;d=0;e=0;i=0;
+        levelwin=0;
+        lock=0;
+        
+        picTimer = new Timer(pictime,new updatePic());
+        picTimer.start();
     }
     
-    public void changeImage(){
-        //Counter
-        int rnd;
-    //    for(int i=0;i<maxpics;i++)
-    //    {
-            if(i%2==0)
-            {
-               rnd=(int)(Math.random()*3)+1;
-               switch(rnd)
-               {
-                   case 1:
-                       imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/a.png")));
-                       controlPannel.add(imageLabel);
-                       break;
-                   case 2:
-                       imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/b.png")));
-                       controlPannel.add(imageLabel);
-                       break;
-                   case 3:
-                       imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/c.png")));
-                       controlPannel.add(imageLabel);
-                       break;     
-               }
-            }//end if
-            else
-            {          
-                rnd=(int)(Math.random()*2)+1;
-                switch(rnd)
-                {
-                   case 1:
-                       imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/d.png")));
-                       controlPannel.add(imageLabel);
-                       break;
-                   case 2:
-                        imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/e.png")));
-                        controlPannel.add(imageLabel);
-                        break;
-                   default:
-                        break;
-                }
-            }//end else
-            controlPannel.repaint();
-          
-            i++;
-    //    }//end for
-    }//end method changeImage
-    
-    class ImageTry implements Runnable
-    {
-        
+    public class updatePic implements ActionListener{
         @Override
-        public void run() 
-        {
-          a=0;b=0;c=0;d=0;e=0;i=0;
-          changeImage();
-          Timer
-        }//end run
+        public void actionPerformed(ActionEvent arg0) {
+            int rnd=0;
+            if(i==0){
+                imageLabel.setIcon(null);
+            }
+            if(i>0&&i<(maxpics*2)+1){
+                if(i%2==0){//no image for even times
+                    imageLabel.setIcon(null);
+                }
+                else
+                {
+                   
+                   rnd=(int)(Math.random()*5)+1;
+                   switch(rnd)
+                   {
+                       case 1:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/a.png")));
+                           a++;
+                           break;
+                       case 2:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/b.png")));
+                           b++;
+                           break;
+                       case 3:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/c.png")));
+                           c++;
+                           break;     
+                       case 4:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/d.png")));
+                           d++;
+                           break;
+                       case 5:
+                            imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/e.png")));
+                            e++;
+                            break;
+                       default:
+                            break;
+                    }
+                }//end else
+            }
+            
+            if(i==maxpics*2+1){
+                try { Thread.sleep(2000);  } catch (InterruptedException ex) {  }   
+            }
+            if(i==maxpics*2+2){
+               picTimer.stop();
+               question(); 
+            }
+            i++;
+        }
+        
+    }
+    
+    public void question(){
+        ansTextField.setVisible(true);
+        submitButton.setVisible(true);
+        questionLabel.setVisible(true);
+        
+        int rnd = (int)(Math.random()*5)+1;
+        switch(rnd)
+                   {
+                       case 1:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/a.png")));
+                           ans=a;
+                           break;
+                       case 2:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/b.png")));
+                           ans=b;
+                           break;
+                       case 3:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/c.png")));
+                           ans=c;
+                           break; 
+                       case 4:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/d.png")));
+                           ans=d;
+                           break;
+                       case 5:
+                           imageLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/e.png")));
+                           ans=e;
+                           break;
+                   }
+        System.out.println(ans);
         
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         heading = new javax.swing.JLabel();
@@ -150,6 +183,8 @@ public class GamePannel extends javax.swing.JPanel {
         submitButton = new javax.swing.JButton();
         helpButton = new javax.swing.JButton();
         levelLabel = new javax.swing.JLabel();
+        questionLabel = new javax.swing.JLabel();
+        ansTextField = new javax.swing.JTextField();
         footerLabel = new javax.swing.JLabel();
         background_Image = new javax.swing.JLabel();
 
@@ -159,7 +194,7 @@ public class GamePannel extends javax.swing.JPanel {
         heading.setForeground(new java.awt.Color(255, 25, 255));
         heading.setText("Memory Game");
         add(heading);
-        heading.setBounds(220, 0, 360, 90);
+        heading.setBounds(140, 10, 430, 90);
 
         javax.swing.GroupLayout controlPannelLayout = new javax.swing.GroupLayout(controlPannel);
         controlPannel.setLayout(controlPannelLayout);
@@ -192,7 +227,7 @@ public class GamePannel extends javax.swing.JPanel {
             }
         });
         add(submitButton);
-        submitButton.setBounds(670, 360, 110, 23);
+        submitButton.setBounds(440, 490, 110, 23);
 
         helpButton.setText("help");
         helpButton.addActionListener(new java.awt.event.ActionListener() {
@@ -201,14 +236,29 @@ public class GamePannel extends javax.swing.JPanel {
             }
         });
         add(helpButton);
-        helpButton.setBounds(670, 440, 110, 23);
+        helpButton.setBounds(670, 350, 110, 23);
 
         levelLabel.setFont(new java.awt.Font("TakaoPGothic", 1, 24)); // NOI18N
-        levelLabel.setForeground(new java.awt.Color(252, 236, 236));
         levelLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        levelLabel.setText("Level : 1/2");
+        levelLabel.setText("Level : 1/3");
         add(levelLabel);
         levelLabel.setBounds(630, 140, 180, 50);
+
+        questionLabel.setFont(new java.awt.Font("TakaoPGothic", 1, 18)); // NOI18N
+        questionLabel.setForeground(new java.awt.Color(230, 116, 26));
+        questionLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        questionLabel.setText("how many times this pic appeared :");
+        add(questionLabel);
+        questionLabel.setBounds(80, 430, 350, 50);
+        questionLabel.getAccessibleContext().setAccessibleDescription("");
+
+        ansTextField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                ansTextFieldKeyTyped(evt);
+            }
+        });
+        add(ansTextField);
+        ansTextField.setBounds(460, 450, 70, 30);
 
         footerLabel.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
         footerLabel.setForeground(new java.awt.Color(255, 244, 244));
@@ -219,33 +269,67 @@ public class GamePannel extends javax.swing.JPanel {
         background_Image.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/gamePannelBackground.jpg"))); // NOI18N
         add(background_Image);
         background_Image.setBounds(0, 0, 820, 620);
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void newButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newButtonActionPerformed
+    private void newButtonActionPerformed(java.awt.event.ActionEvent evt) {                                          
+        picTimer.stop();
         initBoard();
         controlPannel.grabFocus();//get focus back to control pannel
-    }//GEN-LAST:event_newButtonActionPerformed
+    }                                         
 
-    private void submitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitButtonActionPerformed
-            updateBox();
-            controlPannel.grabFocus();//get focus back to control pannel
-        
-    }//GEN-LAST:event_submitButtonActionPerformed
-
-    private void helpButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_helpButtonActionPerformed
-        JOptionPane.showMessageDialog(controlPannel,"* use W A S D keys to move player\n"
-                                            +"* Red dots are your target point\n"
-                                            +"* Yellow boxes are to be placed on target points (Red Dot)\n"
-                                            +"* you can undo only the last move\n"
-                                            +"* There are total 3 levels\n"
-                                            +"* Each level carries 10 points\n"
+    private void helpButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
+        JOptionPane.showMessageDialog(controlPannel,"* count number of times every pic is shown\n"
+                                            +"* at end a picture wil be displayed\n"
+                                            +"* enter number of times it was displayed\n"
+                                            +"* you can press enter or click submit button to submit ans\n"
+                                            +"* you can submit ans only once if wrong then reset level\n"
                                             ,"Instructions"
                                             ,JOptionPane.INFORMATION_MESSAGE                                    
         );
         controlPannel.grabFocus();//get focus back to control pannel
-    }//GEN-LAST:event_helpButtonActionPerformed
+    }                                          
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private void submitButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        if(lock==0){
+            lock=1;
+            System.out.println("listner called");  
+            boolean validans=false;
+            String digit=(ansTextField.getText());
+            if(digit.compareTo("")==0) digit=" "; //to make string length at least one
+            if(digit.charAt(0)>='0' && digit.charAt(0)<='9' && digit.length()==1){
+                validans=true;
+                if(ans==Integer.parseInt(ansTextField.getText())){
+                    questionLabel.setText("Correct Answer !!!");
+                    ansTextField.setVisible(false);
+                    submitButton.setVisible(false);
+                    gameFinish();
+                    ansTextField.setText("");
+                    return;
+                }
+                else {
+                    questionLabel.setText("Wrong Ans .... plz reset ...");
+                    ansTextField.setVisible(false);
+                    submitButton.setVisible(false);
+                    
+                }
+            }
+            if(validans==false){
+                questionLabel.setText("Invalid Ans .... plz reset ...");
+                ansTextField.setVisible(false);
+                    submitButton.setVisible(false);
+            }
+            ansTextField.setText("");
+        }
+    }                                            
+
+    private void ansTextFieldKeyTyped(java.awt.event.KeyEvent evt) {                                      
+        if(evt.getKeyChar()=='\n'){
+            submitButtonActionPerformed(new ActionEvent(stage, d, TOOL_TIP_TEXT_KEY));
+        }
+    }                                     
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JTextField ansTextField;
     private javax.swing.JLabel background_Image;
     private javax.swing.JPanel controlPannel;
     private javax.swing.JLabel footerLabel;
@@ -253,6 +337,7 @@ public class GamePannel extends javax.swing.JPanel {
     private javax.swing.JButton helpButton;
     private javax.swing.JLabel levelLabel;
     private javax.swing.JButton newButton;
+    private javax.swing.JLabel questionLabel;
     private javax.swing.JButton submitButton;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
